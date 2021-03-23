b9038e0779aec95bf2bf6d378b9fc4430244a7c0
Lewis <davanzo.lewis@gmail.com>
2021-03-23 13:54:40 -0400

    add vendor prefixes and new class names

--- src/SimpleParallax.svelte
+++ src/SimpleParallax.svelte
@@ -19,24 +19,25 @@
   });
 
   $: intersecting = y > top - height && y < top + height;
-  $: if (intersecting && !disabled) translate = `transform: translate3d(0, ${-(y - top) * rate}px, 0)`;
+  $: if (intersecting && !disabled) translate = `translate3d(0, ${-(y - top) * rate}px, 0)`;
 </script>
 
 <svelte:window bind:scrollY={y} on:resize={getTop} />
 
-<div class="parallax-container" bind:this={container} bind:offsetHeight={height}>
-  <div class="parallax-item" style={translate}>
+<div class="simple-parallax-container" bind:this={container} bind:offsetHeight={height}>
+  <div class="simple-parallax-item" style="-ms-transform: {translate}; -webkit-transform: {translate}; transform: {translate};">
     <slot />
   </div>
 </div>
 
 <style>
-  .parallax-container {
+  .simple-parallax-container {
     height: 100%;
     width: 100%;
     overflow: hidden;
   }
-  .parallax-item {
+  .simple-parallax-item {
+    position: relative;
     height: 100%;
     width: 100%;
   }
