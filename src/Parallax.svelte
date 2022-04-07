c7c193b70499ab9db722ac83446075f54d3831bf
Nathan Cahill <nathan@nathancahill.com>
2022-04-06 21:00:31 -0400

    remove opinionated styles

--- src/Parallax.svelte
+++ src/Parallax.svelte
@@ -148,12 +148,7 @@
 
 <style>
   .parallax-container {
-    width: 100%;
-    position: relative;
     overflow: hidden;
-    -ms-transform: translate3d(0, 0, 0);
-    -webkit-transform: translate3d(0, 0, 0);
-    transform: translate3d(0, 0, 0);
     box-sizing: border-box;
   }
 </style>
